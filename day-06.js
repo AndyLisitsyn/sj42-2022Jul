4e7838053a8cc809c458204954492ee1f802ad03
Andrii Lisitsyn <andrii.lisitsyn@gmail.com>
2022-07-21 09:16:18 +0300

    day-06

---
+++ day-06.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/maximum-multiple/train/javascript
+
+function maxMultiple(divisor, bound) {
+  return bound - (bound % divisor);
+}
+
+//https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+
+function hero(bullets, dragons) {
+  return bullets >= dragons * 2;
+}
+
+//https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+function unusualFive() {
+  return ["one", "two", "three", "four", "five"].length;
+}
