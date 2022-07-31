8321d13c7913cae58ad4a988756f308c7505a059
Andrii Lisitsyn <andrii.lisitsyn@gmail.com>
2022-07-31 17:39:39 +0300

    minor improvements

--- day-14.js
+++ day-14.js
@@ -1,10 +1,10 @@
 function match(candidate, job) {
-  for (const jobSkill of job.skills) {
-    if (
-      candidate.skills.some(
-        (skill) => jobSkill.name === skill.name && skill.preference === "avoid"
-      ) ||
+  return job.skills.every(
+    (jobSkill) =>
       !candidate.skills.some(
+        (skill) => jobSkill.name === skill.name && skill.preference === "avoid"
+      ) &&
+      candidate.skills.some(
         (skill) =>
           (skill.name === jobSkill.name ||
             jobSkill.substitutions.some((sub) => sub.name === skill.name)) &&
@@ -12,9 +12,5 @@ function match(candidate, job) {
             (skill.preference === "desired" &&
               jobSkill.idealYears <= skill.experience * 1.5))
       )
-    ) {
-      return false;
-    }
-  }
-  return true;
+  );
 }
