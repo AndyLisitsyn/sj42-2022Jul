6364670ab8f1aaa88f1e7b739ab1fe2821cb1f0e
Andrii Lisitsyn <andrii.lisitsyn@gmail.com>
2022-08-07 14:56:02 +0300

    add 1st kata

---
+++ day-17.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/573023c81add650b84000429/train/javascript
+
+function countGrade(scores) {
+  return {
+    S: scores.filter((x) => x === 100).length,
+    A: scores.filter((x) => x < 100 && x >= 90).length,
+    B: scores.filter((x) => x < 90 && x >= 80).length,
+    C: scores.filter((x) => x < 80 && x >= 60).length,
+    D: scores.filter((x) => x < 60 && x >= 0).length,
+    X: scores.filter((x) => x === -1).length,
+  };
+}
