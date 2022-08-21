0e647bb2b3f342037d89e1aee9415dcd8743dfbf
Andrii Lisitsyn <andrii.lisitsyn@gmail.com>
2022-08-21 11:19:48 +0300

    add day-28

---
+++ day-28.js
@@ -0,0 +1,7 @@
+// http://www.codewars.com/kata/a-function-within-a-function
+
+const always = (n) => () => n;
+
+// https://www.codewars.com/kata/59bdbe9d46038724ca0000b9/train/javascript
+
+const solution = (arr, options) => arr.map((x) => x + 2 * options.modifier);
