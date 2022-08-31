94ba5c1bb644317c3a066aabc9fcb20e52e2a5f9
Andrii Lisitsyn <andrii.lisitsyn@gmail.com>
2022-08-31 14:34:58 +0300

    day-31

---
+++ day-31.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/534fcca5edb124cfe6000f60/train/javascript
+
+const Singleton = function () {
+  if (Singleton.__instance) {
+    return Singleton.__instance;
+  }
+  Singleton.__instance = this;
+};
+
+// https://www.codewars.com/kata/588a00ad70720f2cd9000005/train/javascript
+
+class Router {
+  constructor() {
+    this.routes = new Map();
+  }
+
+  bind(url, method, action) {
+    this.routes.set(url + ":" + method, action);
+  }
+
+  runRequest(url, method) {
+    if (!this.routes.has(url + ":" + method)) {
+      return "Error 404: Not Found";
+    }
+    return this.routes.get(url + ":" + method)();
+  }
+}
