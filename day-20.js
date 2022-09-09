b037f65e7a1708206b1495314b64343d4de59e9c
Andrii Lisitsyn <andrii.lisitsyn@gmail.com>
2022-09-09 13:04:29 +0300

    day-20

---
+++ day-20.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/573bca07dffc1aa693000139/train/javascript
+
+const regex = new RegExp("^-?9.*0{4,}$");
+
+// https://www.codewars.com/kata/573d11c48b97c0ad970002d4/train/javascript
+
+const regex = /https?:\/\/[a-z0-9\.]+\.(com|net)/gi;
